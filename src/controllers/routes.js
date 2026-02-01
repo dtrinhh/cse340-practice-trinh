4cb4754945fa1f405c9faf2a621e446967a3cc32
dtrinhh <77747120+dtrinhh@users.noreply.github.com>
2026-02-01 14:29:49 -0700

    Added all folders, files, and code for MVC structure

---
+++ src/controllers/routes.js
@@ -0,0 +1,27 @@
+// import statements for controllers and middleware
+import { Router } from 'express';
+import { addDemoHeaders } from '..middleware/demo/headers.js';
+import { catalogPage, courseDetailPage } from './catalog/catalog';
+import { homePage, aboutPage, demoPage, testErrorPage } from './index.js'
+
+// Create a new router instance
+const router = Router();
+
+// Route definitions
+
+// Home and about page
+router.get('/', homePage);
+router.get('/about', aboutPage);
+
+// Course catalog pages
+router.get('/catalog', catalogPage);
+router.get('/catalog/:courseId', courseDetailPage);
+
+// Demo page with special middleware
+router.get('/demo', addDemoHeaders, demoPage);
+
+// Route for error page
+router.get('/test-error', testErrorPage);
+
+
+export default router;
